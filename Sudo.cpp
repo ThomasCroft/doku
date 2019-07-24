5d0a2624f5318c79f0eac15f83419c656774a800
ThomasCroft <40888593+ThomasCroft@users.noreply.github.com>
2019-07-24 14:03:39 +0100

    Update Sudo.cpp
    sort out whitespace

--- Sudo.cpp
+++ Sudo.cpp
@@ -15,29 +15,29 @@ int main(int argc, char *argv[])
   }
 
   const std::string inputFilename = argv[1];
-	std::cout << "Input filename: " << inputFilename << std::endl;
+  std::cout << "Input filename: " << inputFilename << std::endl;
 
   std::ifstream ifs;
   //ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
   ifs.ignore(9);
 
-	try
-	{
+  try
+  {
     ifs.open(inputFilename);
-	}
-	catch(const std::ifstream::failure &e)
-	{
+  }
+  catch(const std::ifstream::failure &e)
+  {
     std::cout << "Caught exception: " << e.what() << std::endl;
     return 1;
-	}
+  }
 
-	Base sudo{ifs};
-	auto start = std::chrono::steady_clock::now();
-	std::cout << "Solve by backtrack" << std::endl;
-	sudo.solveByBackTrack();
-	auto end = std::chrono::steady_clock::now();
-	auto diff = end - start;
-	std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
-	std::cout << sudo << std::endl;
-	return 0;
+  Base sudo{ifs};
+  auto start = std::chrono::steady_clock::now();
+  std::cout << "Solve by backtrack" << std::endl;
+  sudo.solveByBackTrack();
+  auto end = std::chrono::steady_clock::now();
+  auto diff = end - start;
+  std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
+  std::cout << sudo << std::endl;
+  return 0;
 }
