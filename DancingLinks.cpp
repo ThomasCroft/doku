dbe78d113a24fddd7fc0ec9018f54fb6ae029e66
Thomas Croft <thomas@ecroft.com>
2021-03-13 10:58:14 +0000

    Add a cover function

--- DancingLinks.cpp
+++ DancingLinks.cpp
@@ -4,7 +4,8 @@ DancingLinks::DancingNode::DancingNode() :
   m_up(this),
   m_down(this),
   m_left(this),
-  m_right(this)
+  m_right(this),
+  m_column()
 {
 }
 
@@ -30,4 +31,28 @@ void DancingLinks::DancingNode::reattachLeftRight()
 {
   m_left->m_right = this;
   m_right->m_left = this;
+}
+
+DancingLinks::ColumnHeader::ColumnHeader(const std::string& identifier) :
+  DancingNode(),
+  m_size(),
+  m_identifier(identifier)
+{
+}
+
+void DancingLinks::ColumnHeader::cover()
+{
+  for (auto* columnLink = m_down; columnLink != this; columnLink = columnLink->m_down)
+  {
+    for (auto* rowLink = columnLink->m_right; rowLink != columnLink; rowLink = rowLink->m_right)
+    {
+      rowLink->detachUpDown();
+      --(rowLink->m_column->m_size);
+    }
+  }
+}
+
+void DancingLinks::ColumnHeader::uncover()
+{
+
 }
--- DancingLinks.h
+++ DancingLinks.h
@@ -1,20 +1,40 @@
 #pragma once
+#include <string>
+
 class DancingLinks
 {
-  class DancingNode final
+  class ColumnHeader;
+  class DancingNode
   {
   public:
+    DancingNode();
+
     void detachUpDown();
     void detachLeftRight();
 
     void reattachUpDown();
     void reattachLeftRight();
 
-  private:
+  public:
     DancingNode* m_up;
     DancingNode* m_down;
     DancingNode* m_left;
     DancingNode* m_right;
+    ColumnHeader* m_column;
+  };
+
+  class ColumnHeader : public DancingNode
+  {
+  public:
+    ColumnHeader(const std::string& identifier);
+
+    void cover();
+    void uncover();
+
+  private:
+    size_t m_size;
+    std::string m_identifier;
+
   };
 };
 
